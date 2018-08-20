f14411b7a49b783e98298ad042df96f0be177896
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-08-20 15:01:21 +0200

    Updating os-info script

--- custiw/scripts/os-info.ps1
+++ custiw/scripts/os-info.ps1
@@ -2,9 +2,8 @@
 
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory=$True,ValueFromPipeline=$True,Position=0)][string]$property
+    [Parameter(Mandatory=$False,ValueFromPipeline=$True,Position=0)][string]$property
 );
-
 
 function Get-OS-Version {
     Get-WmiObject Win32_OperatingSystem | Select-Object -ExpandProperty Caption
@@ -14,8 +13,25 @@ function Get-OS-ProductID {
     Get-WmiObject Win32_OperatingSystem | Select-Object -ExpandProperty SerialNumber
 }
 
+function Get-OS-Full {
+    $win32os_attrs = @('Caption', 'SerialNumber', 'InstallDate', 'LastBootUpTime' )
+    $win32os_rdata = Get-WmiObject Win32_OperatingSystem -Property ($win32os_attrs) |
+    		     Select-Object -Property ($win32os_attrs)
+
+    $json_raw = [ordered]@{
+        family = 'Windows'
+        release = $win32os_rdata.Caption
+        boottime = $win32os_rdata.LastBootUpTime
+        productid = $win32os_rdata.SerialNumber
+        installed = $win32os_rdata.InstallDate
+    }
+    $json_raw | ConvertTo-Json
+}
+
 if ($property -eq "version")  {
     Get-OS-Version
 } elseif ($property -eq "productid") {
     Get-OS-ProductID
+} else {
+    Get-OS-Full
 }
